b098dd97fe289f0aab2bc3e42a7b07b4c5fbf6e4
syohei <syohei@syohei-PC>
2017-02-26 21:05:33 +0900

    Factory MethodパターンのMainクラスにコメントを追加

--- DesignPattern/src/pattern/factoryMethod/idcard/Main.java
+++ DesignPattern/src/pattern/factoryMethod/idcard/Main.java
@@ -6,13 +6,19 @@ import pattern.factoryMethod.framework.Product;
 public class Main {
 
 	public static void main(String[] args) {
+		// ファクトリーオブジェクトの生成
 		Factory factory = new IDCardFactory();
+		
+		// ファクトリーを用いてProduct(IDCard)オブジェクトを生成
 		Product card1 = factory.create("田中");
 		Product card2 = factory.create("鈴木");
 		Product card3 = factory.create("佐藤");
+		
+		// IDCardを使用する。
 		card1.use();
 		card2.use();
 		card3.use();
+		
 		System.out.println(((IDCardFactory)factory).getOwners().toString());
 	}
 
