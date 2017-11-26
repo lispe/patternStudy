70b5095279378ff2a35cca1a27aefc5336f14c72
syohei <syohei@syohei-PC>
2017-11-26 15:10:19 +0900

    Flyweightパターン 練習問題20-1,20-2追加

--- DesignPattern/src/pattern/flyweight/BigString.java
+++ DesignPattern/src/pattern/flyweight/BigString.java
@@ -13,6 +13,22 @@ public class BigString {
 		}
 	}
 	
+	public BigString(String string, boolean shared) {
+		bigchars = new BigChar[string.length()];
+		BigCharFactory factory = BigCharFactory.getInstance();
+		
+		if(shared) {
+			for(int i = 0; i < bigchars.length; i++) {
+				bigchars[i] = factory.getBigChar(string.charAt(i));
+			}
+		} else {
+			for(int i = 0; i < bigchars.length; i++) {
+				bigchars[i] = new BigChar(string.charAt(i));
+			}
+		}
+	}
+	
+	
 	public void print() {
 		for(int i = 0; i < bigchars.length; i++) {
 			bigchars[i].print();
--- DesignPattern/src/pattern/flyweight/Main.java
+++ DesignPattern/src/pattern/flyweight/Main.java
@@ -4,9 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		BigString bs = new BigString("010");
-		bs.print();
-
+		long used;
+		
+		BigString bs1 = new BigString("0101010101",true);
+		Runtime.getRuntime().gc();
+		used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+		System.out.println("共有あり 使用メモリ = " + used);
+		
+		BigString bs2 = new BigString("0101010101",false);
+		Runtime.getRuntime().gc();
+		used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+		System.out.println("共有なし 使用メモリ = " + used);
 	}
 
 }
